8d7dc110f3e2d8dbc799935b92b6f1c58d24975d
Jorge Guerra Yerpes <jorgegueyer@gmail.com>
2024-11-20 00:32:44 +0100

    feat: KafkaHandler creation
    - Create the necessary classes to implement a KafkaHandler

--- extensions/observability-extension/runtime/src/main/java/es/jorgegueyer/quarkus/observability/extension/runtime/handler/KafkaHandler.java
+++ extensions/observability-extension/runtime/src/main/java/es/jorgegueyer/quarkus/observability/extension/runtime/handler/KafkaHandler.java
@@ -1,12 +1,22 @@
 package es.jorgegueyer.quarkus.observability.extension.runtime.handler;
 
+import org.jboss.logging.Logger;
 import org.jboss.logmanager.ExtHandler;
+
+
+import java.util.logging.LogRecord;
 
 public class KafkaHandler extends ExtHandler {
 
+    Logger LOGGER = Logger.getLogger(KafkaHandler.class.getName());
 
-    public KafkaHandler(final Appender appender) {
+    public KafkaHandler() {
 
     }
 
+    @Override
+    public void publish(LogRecord record) {
+        LOGGER.info("KafkaHandler - publish");
+        super.publish(record);
+    }
 }
--- extensions/observability-extension/runtime/src/main/java/es/jorgegueyer/quarkus/observability/extension/runtime/recorder/KafkaHandlerRecorder.java
+++ extensions/observability-extension/runtime/src/main/java/es/jorgegueyer/quarkus/observability/extension/runtime/recorder/KafkaHandlerRecorder.java
@@ -1,9 +1,9 @@
 package es.jorgegueyer.quarkus.observability.extension.runtime.recorder;
 
 import es.jorgegueyer.quarkus.observability.extension.runtime.config.KafkaHandlerConfig;
+import es.jorgegueyer.quarkus.observability.extension.runtime.handler.KafkaHandler;
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.annotations.Recorder;
-import org.jboss.logmanager.handlers.AsyncHandler;
 
 import java.util.Optional;
 import java.util.logging.Handler;
@@ -11,11 +11,17 @@ import java.util.logging.Handler;
 @Recorder
 public class KafkaHandlerRecorder {
 
-    public RuntimeValue<Optional<Handler>> initializeHandler(final KafkaHandlerConfig config) {
-        if (!config.enabled) {
+    private final KafkaHandlerConfig kafkaHandlerConfig;
+
+    public KafkaHandlerRecorder(KafkaHandlerConfig kafkaHandlerConfig) {
+        this.kafkaHandlerConfig = kafkaHandlerConfig;
+    }
+
+    public RuntimeValue<Optional<Handler>> initializeHandler() {
+        if (this.kafkaHandlerConfig.enabled()) {
             return new RuntimeValue<>(Optional.empty());
         }
-        AsyncHandler asyncHandler = new AsyncHandler();
+        return new RuntimeValue<>(Optional.of(new KafkaHandler()));
 
     }
 }
