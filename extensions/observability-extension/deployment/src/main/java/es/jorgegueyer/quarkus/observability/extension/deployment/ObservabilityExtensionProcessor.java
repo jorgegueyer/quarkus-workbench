da4aebb18bc4b0d885e8603687d98609755fa852
Jorge Guerra Yerpes <jorgegueyer@gmail.com>
2024-11-20 00:30:29 +0100

    feat: TraceSpan processor and exporter imp
    - Add TraceSpanExporter component to generate traces by each span generated
    - Add TraceSpanProcessor component to generate traces by each span processed
    - Add SPI to instance the TraceSpanExporter through TraceSpanExporterProvider
    - Add TraceSpanRecorder for generate the bytecode for the TraceSpanProcessor

--- extensions/observability-extension/deployment/src/main/java/es/jorgegueyer/quarkus/observability/extension/deployment/ObservabilityExtensionProcessor.java
+++ extensions/observability-extension/deployment/src/main/java/es/jorgegueyer/quarkus/observability/extension/deployment/ObservabilityExtensionProcessor.java
@@ -1,7 +1,12 @@
 package es.jorgegueyer.quarkus.observability.extension.deployment;
 
+import es.jorgegueyer.quarkus.observability.extension.runtime.recorder.KafkaHandlerRecorder;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.LogHandlerBuildItem;
 
 class ObservabilityExtensionProcessor {
 
@@ -10,5 +15,11 @@ class ObservabilityExtensionProcessor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    void registerKafkaHandler(KafkaHandlerRecorder recorder, BuildProducer<LogHandlerBuildItem> logHandler) {
+        logHandler.produce(new LogHandlerBuildItem(recorder.initializeHandler()));
     }
 }
